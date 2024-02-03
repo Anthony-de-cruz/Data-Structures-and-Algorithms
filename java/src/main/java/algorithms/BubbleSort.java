02845a3ff69e6e5230461fde7b574e908655356d
Anthony de Cruz <anthonydecruz@proton.me>
2024-02-03 20:30:58 +0000

    implemented bubble sort test

--- java/src/main/java/algorithms/BubbleSort.java
+++ java/src/main/java/algorithms/BubbleSort.java
@@ -1,19 +1,59 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BubbleSort {
 
+    /**
+     * An implementation of bubble sort
+     *
+     * @param collection The collection to be sorted
+     * @param <T>        Any type that is comparable
+     */
     public static <T extends Comparable<T>> void
     bubbleSort(List<T> collection) {
         for (int x = 0; x < collection.size(); x++) {
             for (int y = 0; y < collection.size() - 1 - x; y++) {
                 if (collection.get(y).compareTo(collection.get(y + 1)) > 0) {
                     T temp = collection.get(y);
+
                     collection.set(y, collection.get(y + 1));
                     collection.set(y + 1, temp);
                 }
             }
+        }
+    }
+
+    /**
+     * An implementation of bubble sort
+     *
+     * @param array The array to be sorted
+     * @param <T>   Any type that is comparable
+     */
+    public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+        bubbleSort(Arrays.asList(array));
+    }
+
+    /**
+     * An implementation of bubble sort
+     *
+     * @param array The array of integers to be sorted
+     */
+    public static void bubbleSort(int[] array) {
+        if (array == null) {
+            throw new NullPointerException();
+        }
+        // Convert to List
+        List<Integer> list = new ArrayList<>(array.length);
+        for (Integer x : array) {
+            list.add(x);
+        }
+        bubbleSort(list);
+        // Apply sorted values to the original array
+        for (int y = 0; y < array.length; y++) {
+            array[y] = list.get(y);
         }
     }
 }
--- java/src/test/java/algorithms/BubbleSortTest.java
+++ java/src/test/java/algorithms/BubbleSortTest.java
@@ -1,9 +1,35 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 import org.junit.Test;
 
 public class BubbleSortTest {
 
+    /**
+     * Test BubbleSort.bubbleSort()
+     */
     @Test
-    public void bubbleSortTest() {}
+    public void bubbleSortTest() {
+
+        List<Integer> unsorted_list = Arrays.asList(2, 3, 1, 5, 8);
+        List<Integer> sorted_list = Arrays.asList(1, 2, 3, 5, 8);
+        BubbleSort.bubbleSort(unsorted_list);
+        assert (Objects.equals(unsorted_list, sorted_list));
+
+        Integer[] unsorted_integer_array = {1, 4, 2, 4, 10, 0};
+        Integer[] sorted_integer_array = {0, 1, 2, 4, 4, 10};
+        BubbleSort.bubbleSort(unsorted_integer_array);
+        assert (Arrays.equals(unsorted_integer_array, sorted_integer_array));
+
+        int[] unsorted_int_array = {1, 3, 2, 100, 0};
+        int[] sorted_int_array = {0, 1, 2, 3, 100};
+        BubbleSort.bubbleSort(unsorted_int_array);
+        assert (Arrays.equals(unsorted_int_array, sorted_int_array));
+
+        List<Integer> empty_array = List.of();
+        BubbleSort.bubbleSort(empty_array);
+    }
 }

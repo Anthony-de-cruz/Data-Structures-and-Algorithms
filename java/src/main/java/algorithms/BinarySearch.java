c6c5c1118f6646546b84cc82f7b333d2b2b3b818
Anthony de Cruz <anthonydecruz@proton.me>
2024-02-03 20:31:07 +0000

    implemented and tested binary search

---
+++ java/src/main/java/algorithms/BinarySearch.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+import java.util.List;
+
+public class BinarySearch {
+
+    public static <T extends Comparable<T>> boolean binarySearch(List<T> list, T term) {
+        int high = list.size();
+        int low = 0;
+
+        while (low < high) {
+            int half = low + Math.floorDiv(high - low, 2);
+            int comparison = term.compareTo(list.get(half));
+
+            if (comparison == 0) {
+                return true;
+            } else if (comparison < 0) {
+                high = half;
+            } else {
+                low = half + 1;
+            }
+        }
+        return false;
+    }
+}

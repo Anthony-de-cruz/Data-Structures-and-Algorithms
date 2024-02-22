f9f16a9e3bd8f0215e72cde75c4570e4f7bec4cc
Anthony de Cruz <anthonydecruz@proton.me>
2024-02-22 12:30:02 +0000

    Implemented selection sort

---
+++ python/src/algorithms/selection_sort.py
@@ -0,0 +1,36 @@
+def selection_sort(array: list[int]) -> None:
+    """This sorting algorithm is O(n^2)"""
+
+    for x in range(len(array) - 1):
+        # Find the next smallest element
+        position = x
+        minimum = array[x]
+        for y in range(x, len(array)):
+            if array[y] < minimum:
+                position = y
+                minimum = array[y]
+
+        # Swap element at smallest position with element at x
+        array[position] = array[x]
+        array[x] = minimum
+
+
+def test() -> bool:
+    """Throws AssertionError if failed."""
+
+    test_1 = [1, 3, 7, 2, 10, 6]
+
+    selection_sort(test_1)
+    assert test_1 == [1, 2, 3, 6, 7, 10]
+
+    return True
+
+
+if __name__ == "__main__":
+    try:
+        if test():
+            print("Test passed!")
+
+    except AssertionError:
+        print("Test failed!")
+        raise
